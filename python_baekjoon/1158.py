0d3ee534a4a684ad2d6b84bb689abd5037e161fa
이흔오 <gms245@naver.com>
2023-07-31 17:55:58 +0900

    commit

--- python_baekjoon/1158.py
+++ python_baekjoon/1158.py
@@ -1,5 +1,10 @@
-# 
+# 원을 돌면서 삭제
 
-member = input()
+member, position = list(map(int, input().split()))
 
+def delete_position(circle, offset):
+    pass
 
+circle = []
+for num in range(1,member+1):
+    circle.append(num)
