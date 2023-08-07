add2032c378633233f26ac6d301e1e244f6b5bf3
이흔오 <gms245@naver.com>
2023-08-08 00:09:11 +0900

    commit

--- python_baekjoon/study/10163.py
+++ python_baekjoon/study/10163.py
@@ -1,31 +1,16 @@
-# 색종이의 면적을 구하는 문제
-from pprint import pprint
+# 주말 추가문제
+
+arr = [[0] * 1001 for _ in range(1001)]
 T = int(input())
-for tc in range(1, 1+T):
-    # sample에 좌표값들을 받아옴
-    sample = [list(map(int, input().split())) for _ in range(T)]
-    arr = [[0]*1001 for _ in range(1001)]
-    # [0, 0, 10, 10]
-    # [2, 2, 6, 6]
-    # j,i 으로부터 j만큼 10 i만큼 10
-    for m in range(T):
-        for i in range(sample[m][0], sample[m][0] + sample[m][2]):
-            for j in range(sample[m][1], sample[m][1]+ sample[m][3]):
-                arr[i][j] = m + 1
 
-    tmp = [0] * (T+1)
+for tc in range(1, T+1):
+    j, i, wid, hig = map(int, input().split())
+    for y in range(i, i + hig):
+        arr[y][j:j+wid] = [tc] * wid
 
+
+for c in range(1, 1+T):
+    count = 0
     for i in range(1001):
-        for j in range(1001):
-            if arr[i][j]:
-                tmp[arr[i][j]] += 1
-
-    for i in range(1, T+1):
-        print(tmp[i])
-
-
-
-
-
-
-
+        count += arr[i].count(c)
+    print(count)
