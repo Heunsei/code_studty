1ee2f320978b7a2469dc268c4572d504baad01d6
이흔오 <gms245@naver.com>
2023-08-22 23:30:40 +0900

    commit

--- algorithm_lecture/tree/tree_1/problem/1232/1232.py
+++ algorithm_lecture/tree/tree_1/problem/1232/1232.py
@@ -1,12 +1,43 @@
 import sys
 sys.stdin = open('input.txt')
-# TestCase
 
-def post
+# 6
+def order(now):
+    if now:
+        order(left[now])
+        order(right[now])
+        if tree[now] == '+':
+            tree[now] = int(tree[left[now]]) + int(tree[right[now]])
+        elif tree[now] == '-':
+            tree[now] = int(tree[left[now]]) - int(tree[right[now]])
+        elif tree[now] == '*':
+            tree[now] = int(tree[left[now]]) * int(tree[right[now]])
+        elif tree[now] == '/':
+            tree[now] = int(tree[left[now]]) // int(tree[right[now]])
+
 
 for tc in range(1, 11):
     N = int(input())
-    node = [0] * (N+1)
+    node = []
+    tree = [0] * (N+1)
     left = [0] * (N+1)
     right = [0] * (N+1)
+
+    for _ in range(N):
+        node.append(input().split())
+
+    for c in node:
+        if len(c) == 4:
+            tree[int(c[0])] = c[1]
+            left[int(c[0])] = int(c[2])
+            right[int(c[0])] = int(c[3])
+        else:
+            tree[int(c[0])] = c[1]
+
+    #print(tree)
+    #print(f'{tc} left', left)
+    #print(f'{tc} right',right)
+
+    order(1)
+    print(f'#{tc} {tree[1]}')
 
