7cb1d23b1e6b637dff5e7ec375b17f87a5dd4dfe
이흔오 <gms245@naver.com>
2023-08-23 15:39:09 +0900

    commit

--- algorithm_lecture2/start/problems/extra3/fishBread/bread.py
+++ algorithm_lecture2/start/problems/extra3/fishBread/bread.py
@@ -7,5 +7,12 @@ for tc in range(1, T+1):
     # M 초동안 K개의 붕어빵
     # N명의 사람이 시간을 두고 방문
     N, M, K = map(int, input().split())
+    book_list = {}
     people = list(map(int, input().split()))
-    print(N,M,K)
+    for man in people:
+        if man not in book_list:
+            book_list.setdefault(man, 1)
+        else:
+            book_list[man] += 1
+
+    print(book_list)
