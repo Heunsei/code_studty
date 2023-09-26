0a9ef873a44b01b7e5837c388fa875edbe488c1b
HeunOh Lee <101114090+Heunsei@users.noreply.github.com>
2023-09-26 18:23:01 +0900

    Update Mystring.cpp

--- CPP/CPP_Language/chapter9/source/stream_operator/Mystring.cpp
+++ CPP/CPP_Language/chapter9/source/stream_operator/Mystring.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cstring>
 #include "Mystring.h"
-
+// 기본생성자
 Mystring::Mystring()
   :str{nullptr}{
     str = new char[1];
     *str = '\0';
 }
-
+// 오버로딩 생성자
 Mystring::Mystring(const char *s)
 :str{nullptr}{
     if(s==nullptr){
@@ -18,24 +18,24 @@ Mystring::Mystring(const char *s)
         strcpy(str, s);
     }
 }
-
+// 복사 생성자
 Mystring::Mystring(const Mystring &source)
 :str{nullptr}{
     str = new char[strlen(source.str) + 1];
     strcpy(str, source.str);
     std::cout <<"Copy constructor used" << std::endl;
 }
-
+// 이동 생성자
 Mystring::Mystring(Mystring &&source)
 :str{source.str}{
     source.str = nullptr;
     std::cout << "Move constructor used" << std::endl;
 }
-
+// 파괴자
 Mystring::~Mystring(){
     delete [] str;
 }
-
+// 대입 연산자
 Mystring &Mystring::operator=(const Mystring &rhs){
     std::cout << "Using copy assignment" << std::endl;
     if(this == &rhs)
@@ -45,7 +45,7 @@ Mystring &Mystring::operator=(const Mystring &rhs){
     strcpy(str, rhs.str);
     return *this;
 }
-
+// 이동 대입 연산자
 Mystring &Mystring::operator=(Mystring &&rhs){
     std::cout << "using move assignment" << std::endl;
     if(this == &rhs)
@@ -62,12 +62,12 @@ void Mystring::display() const {
 
 int Mystring::get_length() const {return strlen(str);}
 const char *Mystring::get_str() const {return str;}
-
+// 출력 오버로딩
 std::ostream &operator<<(std::ostream &os, const Mystring &rhs){
     os << rhs.str;
     return os;
 }
-
+// 입력 오버로딩
 std::istream &operator>>(std::istream &in, Mystring &rhs){
     char *buff = new char[1000];
     in >> buff;
