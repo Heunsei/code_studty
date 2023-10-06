0a745361b93faa88510ba10ddd816f5be5d1f5a0
이흔오 <gms245@naver.com>
2023-10-06 12:41:50 +0900

    commit

--- algorithm_cheatsheet/cpp_ver/floyd_warshall.cpp
+++ algorithm_cheatsheet/cpp_ver/floyd_warshall.cpp
@@ -1,17 +1,47 @@
+// BOJ 11404 플로이드
+// n개의 도시, 다른도시에 도착하는 m개의 버스
+
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
-const int MAX = 1001;
-const int INF = 987654321;
-int T,N,M,X;
-int from, to, distance;
-int graph[MAX][MAX];
-int main(){
-  cin >> T;
-  for(int tc = 1; tc<=T; tc++){
-    // 1번부터 N번까지의 번호, 인수의집은 X 간선의 수 M
-    cin >> N >> M >> X;
 
+const int inf = 1e9;
+int arr[101][101];
+int n,m;
+
+int main(){
+  std::cin >> n >> m;
+  // 크기만큼 배열을 초기화
+  for(int i{0}; i<=n; i++){
+    for(int j{0}; j<=n; j++){
+      arr[i][j] = i==j ? 0 : inf;
+    }
   }
+  // 정점들을 입력받을때 이전값이랑 비교해서 더 작은길을 넣어줌
+  for(int i{0}; i<m; i++){
+    int a,b,c;
+    std::cin >> a >> b >> c;
+    arr[a][b] = min(arr[a][b], c);
+  }
+
+  for(int k{1}; k<=n; k++){
+    for(int i{1}; i<=n; i++){
+      for(int j{1}; j<=n; j++)
+        arr[i][j] = min(arr[i][k] + arr[k][j], arr[i][j]);
+    }
+  }
+
+  for(int i{1}; i<=n; i++){
+    for(int j{1}; j<=n; j++){
+      if(arr[i][j] == inf) std::cout << "0 " ;
+      else std::cout << arr[i][j] << ' ';
+    }
+    std::cout << std::endl;
+  }
+  return 0;
 }
+
+
   
