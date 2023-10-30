c0b2902e71c3e8cd97173a06d59d37eb5703875e
이흔오 <gms245@naver.com>
2023-10-30 16:59:11 +0900

    commit

---
+++ algorithm_workbook/cpp_BOJ/DFS_BFS/7562/7562_2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int width;
+int T, I;
+int arr[305][305];
+// 시작점x,y 끝점
+int x, y, end_x, end_y;
+std::queue<pair<int,int>> q;
+
+int dx[8] = {-1, -2, -2, -1, 1, 2, 2, 1};
+int dy[8] = {-2, -1, 1, 2, -2, -1, 1, 2};
+
+void bfs(int x, int y){
+    q.push({x,y});
+    arr[x][y] = 0;
+    while(!q.empty()){
+
+        int cx = q.front().first;
+        int cy = q.front().second;
+
+        if (cx==end_x && cy==end_y){
+            cout << arr[cx][cy] << "\n";
+            while(!q.empty()) q.pop();
+            return;
+        }
+        q.pop();
+
+        for(int k{0}; k<8; k++){
+            int nx = cx + dx[k];
+            int ny = cy + dy[k];
+
+            if(nx<0 || ny<0 || nx>I || ny>I) continue;
+            if(arr[nx][ny]!=-1) continue;
+
+            q.push({nx,ny});
+            arr[nx][ny] = arr[cx][cy] + 1;
+        }
+    }
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    // 반복횟수 입력
+    std::cin >> T;
+    while(T--){
+        std::cin >> I;
+
+        for(int i{0}; i<I; i++){
+            for(int j{0}; j<I; j++){
+                arr[i][j] = -1;
+            }
+        }
+
+        cin >> x >> y;
+        cin >> end_x >> end_y;
+        bfs(x,y);
+
+        for(int i{0}; i<I; i++){
+            for(int j{0}; j<I; j++){
+                arr[i][j] = -1;
+            }
+        }
+    }
+
+}
---
+++ algorithm_workbook/cpp_BOJ/tree/23835/23835.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 방의 개수, 쿼리의 수
+int N, N_Q;
+// 쿼리의 종류(1,2), 시작지점, 끝지점
+int query ,start, target;
+vector<int> tree[1001];
+int milk[1001] = {0, };
+bool visited[1001];
+
+// 출발하는 방을 제외하고 i번째로 방문하는 방에 i개의 우유 전달
+
+void solve(int start, int target, int count){
+    visited[start] = true;
+    if (start==target){
+        milk[target];
+        return;
+    }
+
+    for(int next : tree[start]){
+        if (visited[next]) continue;
+        milk[next] += count+1;
+        visited[next] = true;
+        solve(next, target, count+1);
+    }
+}
+
+
+int main(){
+    // 방의 개수 입력
+    std::cin >> N;
+    
+    for(int i{0}; i<N-1; i++){
+        // 양뱡향 연결
+        cin >> start >> target;
+        tree[start].push_back(target);
+        tree[target].push_back(start);
+    }
+    
+    std::cin >> N_Q;
+    
+    for(int i{0}; i<N_Q; i++){
+        std::cin >> query >> start;
+        if (query == 1){
+            std::cin >> target;
+            // 방문배열 초기화
+            for(int i{0}; i<N+1; i++) visited[i] = false;
+            solve(start, target, 0);
+        }else{
+            std::cout << milk[start] << std::endl;
+        }
+    }
+    return 0;
+}
