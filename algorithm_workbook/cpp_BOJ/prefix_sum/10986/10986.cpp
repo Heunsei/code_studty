c84e33bc7f27a6908bc5039846d79e18fb243958
이흔오 <gms245@naver.com>
2023-11-05 00:28:55 +0900

    commit

---
+++ algorithm_workbook/cpp_BOJ/prefix_sum/10986/10986.cpp
@@ -0,0 +1,40 @@
+// 나머지 합 구하기
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N,M;
+    cin >> N >> M;
+    vector<long> S(N,0);
+    vector<long> C(M,0);
+    long answer = 0;
+    cin >> S[0];
+
+    for (int i{1}; i<N; i++){
+        int temp = 0;
+        cin >> temp;
+        S[i] = S[i-1] + temp;
+    }
+
+    for (int i{0}; i<N; i++){
+        int temp = S[i] % M;
+        if (temp==0){
+            answer++;
+        }
+        C[temp]++;
+    }
+
+    for(int i{0}; i<M; i++){
+        if(C[i] > 1){
+            answer = answer + (C[i] * (C[i]-1) / 2);
+        }
+    }
+    cout << answer << '\n';
+    return 0;
+}
