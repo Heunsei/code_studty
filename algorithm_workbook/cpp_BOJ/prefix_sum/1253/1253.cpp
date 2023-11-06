39dad5612e928e0064661c8b4fbe258608541e13
이흔오 <gms245@naver.com>
2023-11-07 00:45:07 +0900

    commit

---
+++ algorithm_workbook/cpp_BOJ/prefix_sum/1253/1253.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std
+
+int main(){
+    int N;
+    std::vector<long long> arr(N, 0);
+    for (int i{0}; i<N; i++){
+        std::cin >> arr[i];
+    }
+    
+    sort(arr.begin(), arr.end());
+
+    int start_index = 0;
+    int end_index = 0;
+    int check_index = 1;
+    int cnt = 0;
+
+    while (check_index < N){
+        if (arr[start_index] + arr[end_index] == arr[check_index]){
+            cnt++;
+        }
+        if         
+    }
+    
+
+}
