507f97343df9bd5b7cc372ba94d3ff94f28d99c1
이흔오 <gms245@naver.com>
2023-11-09 01:11:04 +0900

    commit

---
+++ algorithm_workbook/cpp_BOJ/two_pointer/2467/2467.cpp
@@ -0,0 +1,44 @@
+// 특성값이 0에 가까운 두 용액을 만들기
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    vector<int> arr(N, 0);
+
+    for (int i{0}; i<N; i++){
+        cin >> arr[i];
+    }
+
+
+    int i = 0;
+    int j = N-1;
+    int result = abs(arr[i] + arr[j]);
+    pair<int,int> position = {arr[i], arr[j]}; 
+
+    while(i < j){
+        int tmp = arr[i] + arr[j];
+        if (abs(tmp) < result){
+            result = abs(tmp);
+            position = {arr[i] , arr[j]};
+        }
+        if (tmp < 0){
+            i++;
+        }else {
+            j--;
+        }
+    }
+
+    int a = position.first;
+    int b = position.second;
+    (a > b) ? cout << b << " " << a : cout << a << " " << b;
+
+    return 0;
+}
