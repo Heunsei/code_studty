c0f2bcf047004913ab1390d8995e1556858b4f57
이흔오 <gms245@naver.com>
2023-11-13 01:02:54 +0900

    commit

--- algorithm_workbook/cpp_BOJ/two_pointer/17609/17809.cpp
+++ algorithm_workbook/cpp_BOJ/two_pointer/17609/17809.cpp
@@ -5,94 +5,35 @@
 
 using namespace std;
 
-void checkString(string& str){
-    auto i = str.begin();  // 포인터 시작점
-    auto j = str.end()-1;  // 포인터 종료지점
-    int type = 0;
-    int canDelete = true;
-    while (i<=j){
-        if (i==j){
-            cout << type << '\n';
-            return;
+int checkString(string::iterator left, string::iterator right, bool change, string &str){
+    while (left < right) {
+        if (*left == *right){ // 둘이 같으면 안쪽으로
+            left++;
+            right--;
         }
-        // cout << "i: " << *i << " j: " << *j << '\n';
-        if(*i == *j){   // 둘의 값이 같다면 포인터를 중앙쪽으로 배치
-            i++;
-            j--;
-        }
-        else{   // 둘의 값이 다르다면 한번옮겨보고 결정해야함
-            if (canDelete){ // 바꿀수있는 기회가 있다면 한번씩 옮겨보고 다음으로 진행
-                auto next_i = i+1;
-                auto next_j = j-1;
-                auto save_i = i;
-                auto save_j = j;
-                if (*next_i == *j){ // 분기 두개를 모두 확인해야함
-                    canDelete = false;
-                    i+=2;
-                    j--;
-                    type = 1;
-                    while (i<j){
-                        if (*i==*j){
-                            j--;
-                            i++;
-                        }else if (i+1 == j || j-1 == i){
-                            
-                        }
-                        else{
-                            type = 2;
-                        }
-                        if (type==1){
-                            cout << type << '\n';
-                            return;
-                        }else break;
-                    }
-                }
-                i = save_i;
-                j = save_j;
-                if (*next_j == *i){
-                    canDelete = false;
-                    j-=2;
-                    i++;
-                    type = 1;
-                    while (i<j){
-                        if (*i==*j){
-                            j--;
-                            i++;
-                        }
-                        else if (i+1 == j || j-1 == i){
-                            cout << type << '\n';
-                            return;
-                        }
-                        else{
-                            type = 2;
-                        }
-                        cout << type << '\n';
-                        return;
-                    }
-                }
-                if (*next_j != *i || *next_i != *j) {
-                    cout << 2 << '\n';
-                    return;
-                }
-            } else {    // 바꿀수도 없는데 다르다? 2를 출력하고 종료
-                cout << '2' << '\n';
-                return;
+        else{
+            if (change) {
+                if (checkString(left, right-1, false, str) == 0 ||  checkString(left+1, right, false, str) == 0) return 1;
             }
+            return 2; 
         }
     }
-    cout << type << '\n';
-    return;
+    return 0;
 }
 
 int main(){
     ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int N;
     cin >> N;
-    for (int i{0}; i<N; i++){
+    for (int i{0}; i < N; i++){
         string temp;
         cin >> temp;
-        checkString(temp);
+        auto left = temp.begin();    // 포인터 시작점
+        auto right = temp.end() - 1; // 포인터 종료지점
+        int result = checkString(left, right, true, temp);
+        cout << result << '\n';
     }
     return 0;
 }
