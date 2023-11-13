656e2f1c501c8fad7b8196608c384b50fbf53f62
이흔오 <gms245@naver.com>
2023-11-14 00:40:03 +0900

    commit

--- algorithm_workbook/cpp_BOJ/two_pointer/11003/11003.cpp
+++ algorithm_workbook/cpp_BOJ/two_pointer/11003/11003.cpp
@@ -1,35 +1,13 @@
-// BOJ : 11003 최솟값 찾기
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
     int N, L;
-    std::cin >> N >> L;
-    vector<int> arr (N, 0);
-    vector<int> D (N, 0);
-
-    for (int i{0}; i<N; i++){
-        cin >> arr[i];
-    }
-
-
-    for(int i{0}; i<L; i++){
-        if (i==0) D[i] = arr[i];
-        D[i] = min(D[i-1], arr[i]);
-    }
-
-
-    int j = 0;
-    while(j < N){
-        int i;
-        if(j-L < 0) i = 0;
-        else i = j-L;
-        
-
-    }
+    cin >> N >> L;
+    
 
     return 0;
 }
