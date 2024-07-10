148679cefec1f932213caf207c96e7c1a278d636
이흔오 <gms245@naver.com>
2024-07-10 23:05:43 +0900

    commit

---
+++ algorithm_workbook/cpp_BOJ/sort/1377/1377.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    vector<pair<int, int>> vec(N);
+
+    for(int i{0}; i<N; i++){
+        cin >> vec[i].first;
+        vec[i].second = i;
+    }
+
+    sort(vec.begin(), vec.end());
+    int MAX = 0;
+    for(int i{0}; i<N; i++){
+        if(MAX < vec[i].second - i){
+            MAX = vec[i].second - i;
+        }
+    }
+
+    cout << MAX + 1;
+
+    return 0;
+}
